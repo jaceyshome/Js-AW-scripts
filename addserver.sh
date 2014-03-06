021496a340b70d9a6cd1db20d715cfb6006492c0
Jacob Wang <jacob.wang@change2.com.au>
2014-03-06 15:49:04 +1100

    init

---
+++ addserver.sh
@@ -0,0 +1,97 @@
+read -p "enter server url: " url
+read -p "re-enter server url: " urlcheck
+
+if [ "$url" == "" ]; then
+			 echo "error: url is empty"
+			 exit 0
+elif [ "$urlcheck" == "" ]; then
+			 echo "error: url doesn't match"
+			 exit 0
+elif [ "$url" != "$urlcheck" ]; then
+			 echo "error: url doesn't match"
+			 exit 0
+else
+			 echo "Server name: $url"
+fi
+
+
+read -p "Use subfolder for web root? (y/n) " subfolder
+
+if [ "$subfolder" == "y" ] || [ "$subfolder" == "Y" ]; then
+			 subfolder="bin"
+			 read -p "enter subfolder name, default is 'bin': " subfolder
+			 read -p "re-enter subfolder name, default is 'bin': " subfolderCheck
+
+			 if [ "$subfolder" != "$subfolderCheck" ]; then
+							 echo "sub folder name doesn't match"
+							 exit 0
+			 fi
+
+			 if [ "$subfolder" == "" ] && [ "$subfolderCheck" == "" ]; then
+							 subfolder="bin"
+			 fi
+
+			 echo "subfolder name: $subfolder"
+else
+			 subfolder=0
+fi
+
+
+#If subfolder != 0 create folder for subfolder firstly
+if [ "$subfolder" != 0 ]; then
+			 echo "Create git clone folder: " $url
+			 mkdir "$url"
+			 test -d "./$url"
+fi
+
+
+gitFolderName="$url.git"
+
+echo "Create git folder: " $gitFolderName
+mkdir "$gitFolderName"
+test -d "./$gitFolderName"
+
+cd "./$gitFolderName"
+git init --bare
+
+test -d "./hooks"
+cd "./hooks/"
+
+echo "Create post-receive inside hooks/"
+
+if [ "$subfolder" != 0 ]; then
+		echo "GIT_WORK_TREE=/home/ec2-user/$url" > "post-receive"
+		echo "export GIT_WORK_TREE" >> "post-receive"
+		echo "git checkout -f" >> "post-receive"
+		echo "cp -r /home/ec2-user/$url/$subfolder/* /var/www/html/$url" >> "post-receive"
+else
+				echo "GIT_WORK_TREE=/var/www/html/$url" > "post-receive"
+				echo "export GIT_WORK_TREE" >> "post-receive"
+				echo "git checkout -f" >> "post-receive"
+fi
+
+chmod +x "./post-receive"
+
+echo "Create folder $url in /var/www/html/"
+sudo mkdir "/var/www/html/$url"
+sudo chown ec2-user "/var/www/html/$url"
+
+echo "Check document root: /var/www/html/$url"
+test -d "/var/www/html/$url"
+
+echo "Add new server to vhost.conf"
+#change vhost.conf owner to ec2-user
+sudo chown ec2-user "/etc/httpd/conf.d/vhost.conf"
+
+echo "" >> "/etc/httpd/conf.d/vhost.conf"
+echo "<VirtualHost *>" >> "/etc/httpd/conf.d/vhost.conf"
+echo "Servername $url" >> "/etc/httpd/conf.d/vhost.conf"
+echo "DocumentRoot /var/www/html/$url" >> "/etc/httpd/conf.d/vhost.conf"
+echo "</VirtualHost>" >> "/etc/httpd/conf.d/vhost.conf"
+
+sudo chown root "/etc/httpd/conf.d/vhost.conf"
+
+echo "Complete"
+echo "Reload apache server conf"
+
+sudo /etc/init.d/httpd reload
---
+++ removeserver.sh
@@ -0,0 +1,141 @@
+read -p "enter server url: " url
+read -p "re-enter server url: " urlcheck
+
+if [ "$url" == "" ]; then
+        echo "error: url is empty"
+        exit 0
+elif [ "$urlcheck" == "" ]; then
+        echo "error: url doesn't match"
+        exit 0
+elif [ "$url" != "$urlcheck" ]; then
+        echo "error: url doesn't match"
+        exit 0
+else
+        echo "Server name: $url"
+fi
+
+read -p "are you sure? (y/n) " confirm
+
+if [ "$confirm" == "n" ] || [ "$confirm" == "N" ]; then
+       exit 0
+fi
+
+gitFolderName="$url.git"
+textColorRed='\033[0;31m'
+textColorGreen='\033[0;32m'
+defaultColor='\033[0m'
+
+printf "Remove git folder: ${gitFolderName}"
+if [ -d "./$gitFolderName" ]; then
+        rm -rf ./$gitFolderName
+        printf " [${textColorGreen}Done${defaultColor}]\n"
+else
+        printf " [${textColorRed}Failed${defaultColor}]"
+        printf " Cannot find the git folder${defaultColor}\n"
+fi
+
+printf "Remove sub folder: ./${url}"
+if [ -d "./$url" ]; then
+        rm -rf ./$url
+        printf " [${textColorGreen}Done${defaultColor}]\n"
+else
+        printf " [${textColorRed}Failed${defaultColor}]"
+        printf " Cannot find the sub folder${defaultColor}\n"
+fi
+
+
+
+printf "Remove folder: /var/www/html/${url}"
+if [ -d "/var/www/html/$url" ]; then
+        sudo rm -rf /var/www/html/$url
+        printf " [${textColorGreen}Done${defaultColor}]\n"
+else
+        printf " [${textColorRed}Failed${defaultColor}]"
+        printf " Cannot find the folder${defaultColor}\n"
+fi
+
+printf "Search and delete record for server in vhost.f\n"
+
+startMarker='<VirtualHost'
+endMarker='</VirtualHost>'
+vhostFile='/etc/httpd/conf.d/vhost.conf'
+
+#check vhost.f whether it contains the record for vhost.f
+urlLines=( $(grep -n $url$ $vhostFile | cut -f1 -d:) )
+urlLinesLength=${#urlLines[@]}
+
+if [ $urlLinesLength == 0 ]; then
+  printf " [${textColorRed}Failed${defaultColor}]\n"
+  printf "No record for server $url on $vhostFile ${defaultColor}\n"
+  #skip if no record
+
+else
+
+  #search startMarkers and endMarkers' line numbers
+
+        startMarkers=( $(grep -Fn $startMarker $vhostFile | cut -f1 -d:) )
+        endMarkers=( $(grep -Fn $endMarker $vhostFile | cut -f1 -d:) )
+
+        startMarkerLength=${#startMarkers[@]}
+        endMarkerLength=${#endMarkers[@]}
+
+        #search line numbers for lines containing server name
+        startUrlLine=${urlLines[0]}
+        endUrlLine=${urlLines[$urlLinesLength-1]}
+
+        #find the delete range
+        #between start line number and end line number
+        for (( i = $startMarkerLength - 1; i >= 0; i-- )); do
+                 if [ ${startMarkers[$i]} -lt $startUrlLine ]; then
+                                 startRange=${startMarkers[$i]}
+                                 break
+                 fi
+        done
+
+        for (( j = 0; j < $endMarkerLength - 1; i++ )); do
+                 if [ ${endMarkers[$i]} -gt $endUrlLine ]; then
+                                 endRange=${endMarkers[$i]}
+                                 break
+                 fi
+        done
+
+        delete='d'
+
+        cat $vhostFile | sed "$startRange,$endRange""$delete" > vhost.tmp
+
+        #check vhost.tmp file
+        #startMarkers line total numbers
+        #should equal to endMarkers line total numbers
+
+        startMarkers=( $(grep -Fn $startMarker $vhostFile | cut -f1 -d:) )
+        endMarkers=( $(grep -Fn $endMarker $vhostFile | cut -f1 -d:) )
+
+        startMarkerLength=${#startMarkers[@]}
+        endMarkerLength=${#endMarkers[@]}
+
+        if [ $startMarkerLength == $endMarkerLength ]; then
+
+                #backup vhost.f
+
+                dateAndTime=$(TZ=AEST date +"%Y-%m-%d-%H-%M-%S")
+                backupFileName="vhost.f_"$dateAndTime
+                cat $vhostFile > ./vhostbackup/$backupFileName
+                printf "Back up vhost.f: ./vhostbackup/$backupFileName\n"
+
+
+                #replace vhost.f by vhost.tmp
+
+                sudo chown ec2-user "$vhostFile"
+                cat vhost.tmp > $vhostFile
+                rm -f vhost.tmp
+                sudo chown root "$vhostFile"
+
+                printf "Remove server record from vhost.f file [${textColorGreen}Done${defaultColor}]\n"
+
+        fi
+fi
+
+echo  "[---------Completed---------]"
+echo  "Reload apache server conf"
+
+sudo /etc/init.d/httpd reload
