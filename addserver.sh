7fe6932637a71d2888e0814ff58912b4cc8b2c16
Jacob Wang <jacob.wang@change2.com.au>
2014-03-06 16:39:01 +1100

    remove shebang line, it doesn't work on ec2 server

--- addserver.sh
+++ addserver.sh
@@ -1,48 +1,46 @@
-#!/bin/sh
 read -p "enter server url: " url
 read -p "re-enter server url: " urlcheck
 
 if [ "$url" == "" ]; then
-			 echo "error: url is empty"
-			 exit 0
+  echo "error: url is empty"
+  exit 0
 elif [ "$urlcheck" == "" ]; then
-			 echo "error: url doesn't match"
-			 exit 0
+  echo "error: url doesn't match"
+  exit 0
 elif [ "$url" != "$urlcheck" ]; then
-			 echo "error: url doesn't match"
-			 exit 0
+  echo "error: url doesn't match"
+  exit 0
 else
-			 echo "Server name: $url"
+  echo "Server name: $url"
 fi
 
 
 read -p "Use subfolder for web root? (y/n) " subfolder
 
 if [ "$subfolder" == "y" ] || [ "$subfolder" == "Y" ]; then
-			 subfolder="bin"
-			 read -p "enter subfolder name, default is 'bin': " subfolder
-			 read -p "re-enter subfolder name, default is 'bin': " subfolderCheck
+  subfolder="bin"
+  read -p "enter subfolder name, default is 'bin': " subfolder
+  read -p "re-enter subfolder name, default is 'bin': " subfolderCheck
 
-			 if [ "$subfolder" != "$subfolderCheck" ]; then
-							 echo "sub folder name doesn't match"
-							 exit 0
-			 fi
+  if [ "$subfolder" != "$subfolderCheck" ]; then
+    echo "sub folder name doesn't match"
+    exit 0
+  fi
+  if [ "$subfolder" == "" ] && [ "$subfolderCheck" == "" ]; then
+    subfolder="bin"
+  fi
+  echo "subfolder name: $subfolder"
 
-			 if [ "$subfolder" == "" ] && [ "$subfolderCheck" == "" ]; then
-							 subfolder="bin"
-			 fi
-
-			 echo "subfolder name: $subfolder"
 else
-			 subfolder=0
+  subfolder=0
 fi
 
 
 #If subfolder != 0 create folder for subfolder firstly
 if [ "$subfolder" != 0 ]; then
-			 echo "Create git clone folder: " $url
-			 mkdir "$url"
-			 test -d "./$url"
+  echo "Create git clone folder: " $url
+  mkdir "$url"
+  test -d "./$url"
 fi
 
 
@@ -63,27 +61,27 @@ echo "Create post-receive inside hooks/"
 
 
 if [ "$subfolder" != 0 ]; then
-		echo "#!/bin/sh" > "post-recieve"
-		echo "while read oldrev newrev refname" >> "post-receive"
-		echo "do" >> "post-receive"
-		echo "    branch=$(git rev-parse --symbolic --abbrev-ref $refname)" >> "post-receive"
-		echo "    echo Update pushed to branch $branch" >> "post-receive"
-		echo "    GIT_WORK_TREE=/home/ec2-user/$url" >> "post-receive"
-		echo "    export GIT_WORK_TREE" >> "post-receive"
-		echo "    git checkout -f $branch" >> "post-receive"
-		echo "    cp -r /home/ec2-user/$url/$subfolder/* /var/www/html/$url/$branch" >> "post-receive"
-		echo "done" >> "post-receive"
+  echo "#!/bin/sh" > "post-recieve"
+  echo "while read oldrev newrev refname" >> "post-receive"
+  echo "do" >> "post-receive"
+  echo "    branch=$(git rev-parse --symbolic --abbrev-ref $refname)" >> "post-receive"
+  echo "    echo Update pushed to branch $branch" >> "post-receive"
+  echo "    GIT_WORK_TREE=/home/ec2-user/$url" >> "post-receive"
+  echo "    export GIT_WORK_TREE" >> "post-receive"
+  echo "    git checkout -f $branch" >> "post-receive"
+  echo "    cp -r /home/ec2-user/$url/$subfolder/* /var/www/html/$url/$branch" >> "post-receive"
+  echo "done" >> "post-receive"
 else
-		echo "#!/bin/sh" > "post-recieve"
-		echo "while read oldrev newrev refname" >> "post-receive"
-		echo "do" >> "post-receive"
-		echo "    branch=$(git rev-parse --symbolic --abbrev-ref $refname)" >> "post-receive"
-		echo "    echo Update pushed to branch $branch" >> "post-receive"
-		echo "    GIT_WORK_TREE=/home/ec2-user/$url" >> "post-receive"
-		echo "    export GIT_WORK_TREE" >> "post-receive"
-		echo "    git checkout -f $branch" >> "post-receive"
-		echo "    cp -r /home/ec2-user/$url/* /var/www/html/$url/$branch" >> "post-receive"
-		echo "done" >> "post-receive"
+  echo "#!/bin/sh" > "post-recieve"
+  echo "while read oldrev newrev refname" >> "post-receive"
+  echo "do" >> "post-receive"
+  echo "    branch=$(git rev-parse --symbolic --abbrev-ref $refname)" >> "post-receive"
+  echo "    echo Update pushed to branch $branch" >> "post-receive"
+  echo "    GIT_WORK_TREE=/home/ec2-user/$url" >> "post-receive"
+  echo "    export GIT_WORK_TREE" >> "post-receive"
+  echo "    git checkout -f $branch" >> "post-receive"
+  echo "    cp -r /home/ec2-user/$url/* /var/www/html/$url/$branch" >> "post-receive"
+  echo "done" >> "post-receive"
 
 fi
 
